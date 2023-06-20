7ff394d8044533f4ff1858519ceca639b79eb459
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-06-20 21:23:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,6 +35,7 @@ export class App extends Component {
     const { good, neutral, bad } = this.state;
 
     const options = Object.keys(this.state);
+
     return (
       <>
         <Section title="Please leave feedback">
